ff36183bb57aeb3f0168495f822eda1747be244e
chaolee <leechao1959@gmail.com>
2019-06-14 15:10:39 -0400

    1. Migrated to AndroidX libraries 2. Changed customized ToolBar to Use default ActionBar from AndroidX library

--- app/src/main/java/com/smile/karaokeplayer/MainActivity.java
+++ app/src/main/java/com/smile/karaokeplayer/MainActivity.java
@@ -1,29 +1,40 @@
 package com.smile.karaokeplayer;
 
 import android.content.Context;
-import android.graphics.Paint;
+import android.graphics.Color;
 import android.os.Build;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.os.Bundle;
-import android.support.v7.view.ContextThemeWrapper;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ContextThemeWrapper;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
 import com.smile.smilelibraries.privacy_policy.PrivacyPolicyUtil;
 import com.smile.smilelibraries.utilities.ScreenUtil;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = new String(".MainActivity");
+    private final String LOG_TAG = new String("MediaSessionCompatTag");
     private final int PrivacyPolicyActivityRequestCode = 10;
 
     private float textFontSize;
     private float fontScale;
-    private android.support.v7.widget.Toolbar supportToolbar;
-    // private ActionBar supportToolbar;
+    // private Toolbar supportToolbar;  // use customized ToolBar
+    private ActionBar supportToolbar;   // use default ActionBar
+
+    private MediaSessionCompat mediaSession;
+    private PlaybackStateCompat.Builder stateBuilder;
 
     private boolean isAutoPlay;
 
@@ -42,17 +53,16 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        supportToolbar = findViewById(R.id.support_toolbar);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            // API less than 21
-        }
-
+        /*
+        // use customized ToolBar
         setSupportActionBar(supportToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         TextView toolbarTitleTextView = findViewById(R.id.toolbarTitleTextView);
         ScreenUtil.resizeTextSize(toolbarTitleTextView, textFontSize, SmileApplication.FontSize_Scale_Type);
+        //
+        */
 
-        /*
+        // use default ActionBar
         setTitle(String.format(Locale.getDefault(), ""));
         supportToolbar = getSupportActionBar();
         TextView titleView = new TextView(this);
@@ -61,7 +71,31 @@ public class MainActivity extends AppCompatActivity {
         ScreenUtil.resizeTextSize(titleView, textFontSize, SmileApplication.FontSize_Scale_Type);
         supportToolbar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         supportToolbar.setCustomView(titleView);
-        */
+        //
+
+        // Create a MediaSessionCompat
+        mediaSession = new MediaSessionCompat(this, LOG_TAG);
+
+        // Enable callbacks from MediaButtons and TransportControls
+        mediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        // Do not let MediaButtons restart the player when the app is not visible
+        mediaSession.setMediaButtonReceiver(null);
+
+        // Set an initial PlaybackState with ACTION_PLAY, so media buttons can start the player
+        stateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(PlaybackStateCompat.ACTION_PLAY | PlaybackStateCompat.ACTION_PLAY_PAUSE);
+        mediaSession.setPlaybackState(stateBuilder.build());
+
+        // MySessionCallback has methods that handle callbacks from a media controller
+        MediaSessionCallback mediaSessionCallback = new MediaSessionCallback();
+        mediaSession.setCallback(mediaSessionCallback);
+
+        // Create a MediaControllerCompat
+        MediaControllerCompat mediaController = new MediaControllerCompat(this, mediaSession);
+        MediaControllerCompat.setMediaController(this, mediaController);
 
         isAutoPlay = false;
     }
@@ -72,11 +106,17 @@ public class MainActivity extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
+        /*
+        // use customized ToolBar
         final int popupThemeId = supportToolbar.getPopupTheme();
         final Context wrapper = new ContextThemeWrapper(this, popupThemeId);
-        // final Context wrapper = supportToolbar.getThemedContext();
-        final float fScale = fontScale;
+        */
 
+        // use default ActionBar
+        final Context wrapper = supportToolbar.getThemedContext();
+        //
+
+        final float fScale = fontScale;
         ScreenUtil.buildActionViewClassMenu(this, wrapper, menu, fScale, SmileApplication.FontSize_Scale_Type);
 
         return true;
@@ -91,7 +131,6 @@ public class MainActivity extends AppCompatActivity {
             // item.isChecked() return the previous value
             isAutoPlay = !isAutoPlay;
             item.setChecked(isAutoPlay);
-            return true;
         }
         if (id == R.id.open) {
 
@@ -107,5 +146,52 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaSession.release();
+    }
+
+    private class MediaSessionCallback extends MediaSessionCompat.Callback {
+
+        @Override
+        public void onPlay() {
+            super.onPlay();
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+        }
+
+        @Override
+        public void onFastForward() {
+            super.onFastForward();
+        }
+
+        @Override
+        public void onRewind() {
+            super.onRewind();
+        }
     }
 }
